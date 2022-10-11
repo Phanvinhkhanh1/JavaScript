6837deebc30cc128cdcb72d101e38c7816628a10
Phan Vinh Khanh <Khanh161297@gmail.com>
2022-10-11 07:58:06 +0700

    Callback in JavaScript

--- Async-Await/callback.js
+++ Async-Await/callback.js
@@ -1,17 +1,54 @@
-let square = (a, b, h) => (a + b) * h / 2;
-console.log("Area : " + square(3, 4, 5));
+// let square = (a, b, h) => (a + b) * h / 2;
+// console.log("Area : " + square(3, 4, 5));
 
 
-let area = (a, b, callback) => {
+let add = (a, b, callback) => {
     setTimeout(() => {
-        if (typeof a != Number || typeof b != Number) {
+        if (typeof a != 'number' || typeof b != 'number') {
             return callback(new Error("Arguments must be a number"));
         }
         return callback(undefined, a + b);
     }, 1000);
 };
 
-area('4', 5, (err, result) => {
-    if (err) return console.log('Error : ' + err);
-    console.log('Result : ' + result);
+let multiply = (a, b, callback) => {
+    setTimeout(() => {
+        if (typeof a != 'number' || typeof b != 'number') {
+            return callback(new Error("Arguments must be a number"));
+        }
+        return callback(undefined, a * b);
+    }, 1000);
+};
+
+let divide = (a, b, callback) => {
+    setTimeout(() => {
+        if (typeof a != 'number' || typeof b != 'number') {
+            return callback(new Error("Arguments must be a number"));
+        }
+        if (b == 0) return callback(new Error("Divide by 0"));
+        return callback(undefined, a / b);
+    }, 1000);
+};
+
+// area('4', 5, (err, result) => {
+//     if (err) return console.log('Error : ' + err);
+//     console.log('Result : ' + result);
+// });
+
+let area = (a, b, h, callback) => {
+    add(a, b, (err, result) => {
+        if (err) return callback(err);
+        multiply(result, h, (err, result) => {
+            if (err) return callback(err);
+            divide(result, 2, (err, result) => {
+                if (err) return callback(err);
+                callback(undefined, result);
+            });
+        });
+    });
+};
+
+area(4, 5, 2, (err, result) => {
+    if (err) return console.log("Error : " + err);
+    console.log("Area : " + result);
 });
